5148cb61f4380752fbd20e94023507fc6f142461
gymacbookpro <187342134222@163.com>
2023-06-09 18:27:07 +0800

    feat:
    study SpringSecurity and JWT

--- src/main/java/com/blood/user/system/BloodUserSystemApplication.java
+++ src/main/java/com/blood/user/system/BloodUserSystemApplication.java
@@ -2,10 +2,8 @@ package com.blood.user.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})  // 排除security安全验证
+@SpringBootApplication
 public class BloodUserSystemApplication {
 
     public static void main(String[] args) {
